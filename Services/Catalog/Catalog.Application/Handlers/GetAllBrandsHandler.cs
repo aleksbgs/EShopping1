0a2fc73ba61a59d1f98a3b0bfa9a547bfbceac51
Aleksandar Markovic <aleksandar.markovic@asseco-see.rs>
2024-03-31 12:22:40 +0200

    SQRS

---
+++ Services/Catalog/Catalog.Application/Handlers/GetAllBrandsHandler.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using AutoMapper;
+using MediatR;
+using Catalog.Core.Repositories;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetAllBrandsHandler : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllBrandsHandler(IBrandRepository brandRepository,IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+        {
+            var brandList = await _brandRepository.GetAllBrands();
+
+            var brandResponseList = _mapper.Map<IList<BrandResponse>>(brandList);
+
+            return brandResponseList;
+
+        }
+    }
+}
