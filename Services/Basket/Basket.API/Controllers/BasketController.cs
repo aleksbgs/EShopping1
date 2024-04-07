dc1c78cbd3699ffb6f9c69fba7f620b3e3fb5069
Aleksandar Markovic <aleksandar.markovic@asseco-see.rs>
2024-04-07 16:14:53 +0200

    done

---
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using Basket.Application.Commands;
+using Basket.Application.Queries;
+using Basket.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    public class BasketController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public BasketController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("[action]/{userName}", Name = "GetBasketByUserName")]
+        [ProducesResponseType(typeof(ShoppingCartResponse), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult<ShoppingCartResponse>> GetBasket(string userName)
+        {
+            var query = new GetBasketByUserNameQuery(userName);
+
+            var basket = await _mediator.Send(query);
+
+            return Ok(basket);
+        }
+
+        [HttpGet]
+        [Route("[action]/{userName}", Name = "DeleteBasketByUserName")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult<ShoppingCartResponse>> DeleteBasketByUserName(string userName)
+        {
+            var query = new DeleteBasketByUserNameCommand(userName);
+            return Ok(await _mediator.Send(query));
+        }
+
+        [HttpPost("CreateBasket")]
+        [ProducesResponseType(typeof(ShoppingCartResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartResponse>> UpdateBasket(
+            [FromBody] CreateShoppingCartCommand createShoppingCartCommand)
+        {
+            var basket = await _mediator.Send(createShoppingCartCommand);
+            return Ok(basket);
+        }
+
+
+    }
+}
--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace Basket.API;
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+}
