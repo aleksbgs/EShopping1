d37d1ca88bfcac7d615f29488d9287383a617cbb
Aleksandar Markovic <aleksandar.markovic@asseco-see.rs>
2024-04-14 19:05:03 +0200

    Ordering Service

---
+++ Services/Ordering/Ordering.Core/Repositories/IAsyncRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Core.Common;
+
+namespace Ordering.Core.Repositories
+{
+    public interface IAsyncRepository<T> where T : EntityBase
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T,bool>> predicate);
+
+        Task<T> GetByIdAsync(int id);
+
+        Task<T> AddAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<T> DeleteAsync(T entity);
+    }
+}
