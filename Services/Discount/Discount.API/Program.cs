773583c19b9017c1f98ed9263f1e4ea5ea973711
Aleksandar Markovic <aleksandar.markovic@asseco-see.rs>
2024-04-14 13:24:41 +0200

    fix done discount service

--- Services/Discount/Discount.API/Program.cs
+++ Services/Discount/Discount.API/Program.cs
@@ -1,6 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+
+using Discount.API;
+using Discount.Infrastructure.Extensions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+
+        host.MigrateDatabase<Program>();
+
+        host.Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+}
+
