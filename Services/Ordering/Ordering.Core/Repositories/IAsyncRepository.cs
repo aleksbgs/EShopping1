18810096af9ec85acf73d4ccfc08ecb290864e16
Aleksandar Markovic <aleksandar.markovic@asseco-see.rs>
2024-04-15 19:34:53 +0200

    Odrer

--- Services/Ordering/Ordering.Core/Repositories/IAsyncRepository.cs
+++ Services/Ordering/Ordering.Core/Repositories/IAsyncRepository.cs
@@ -18,8 +18,8 @@ namespace Ordering.Core.Repositories
 
         Task<T> AddAsync(T entity);
 
-        Task<T> UpdateAsync(T entity);
+        Task UpdateAsync(T entity);
 
-        Task<T> DeleteAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Common;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "aleksbgs"; //TODO: this will replaced with identity server
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "aleksbgs"; //TODO: this will replaced with identity server
+                        break;
+                }
+
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+
+        }
+    }
+}
