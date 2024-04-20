9452af2169e999a520afa952b2bf19211bc28e6c
Aleksandar Markovic <aleksandar.markovic@asseco-see.rs>
2024-04-20 17:52:14 +0200

    fix

--- Services/Catalog/Catalog.Core/Specs/Pagination.cs
+++ Services/Catalog/Catalog.Core/Specs/Pagination.cs
@@ -8,28 +8,21 @@ namespace Catalog.Core.Specs
 {
     public class Pagination<T> where T : class
     {
-
-        public int PageIndex { get; set; }
-
-        public int PageSize { get; set; }
-
-        public long Count { get; set; }
-
-        public IReadOnlyList<T> Data { get; set; }
-
-        public Pagination()
-        {
-
-        }
-
         public Pagination(int pageIndex, int pageSize, int count, IReadOnlyList<T> data)
         {
             PageIndex = pageIndex;
             PageSize = pageSize;
             Count = count;
             Data = data;
-
         }
 
+        public Pagination()
+        {
+        }
+
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public long Count { get; set; }
+        public IReadOnlyList<T> Data { get; set; }
     }
 }
